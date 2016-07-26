1eb2f89b5ef5105a4e2dd2607130906147b9a281
Yuuki221 <xcicada824@gmail.com>
2016-07-26 18:50:04 -0400

    first commit to the theme

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * non-profit-focus Theme Customizer.
+ *
+ * @package non-profit-focus
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function non_profit_focus_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'non_profit_focus_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function non_profit_focus_customize_preview_js() {
+	wp_enqueue_script( 'non_profit_focus_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'non_profit_focus_customize_preview_js' );
