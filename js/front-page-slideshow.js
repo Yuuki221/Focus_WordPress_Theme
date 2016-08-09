ac2ca246029b3511e5b13d3758a28d8d4f1a7f34
Yuuki221 <xcicada824@gmail.com>
2016-08-09 09:32:09 -0400

    add slideshow ~~ finally yh ~~~~

--- js/front-page-slideshow.js
+++ js/front-page-slideshow.js
@@ -1,8 +1,29 @@
-$(function(){
-	$('.slide-container img:gt(0)').hide(0);
-	setInterval(function(){
-		$('.slide-container :first-child').fadeOut()
-			.next('img').fadeIn()
-			.end().appendTo('.slide-continer');
-	}, 3000);
+jQuery(function($){
+	(function($){
+		$.fn.slideshow = function(options){
+			options = $.extend({
+				'timeout' : 3000,
+				'speed' : 400 // normal
+			}, options);
+			// loop through the selected elements,
+			return this.each(function(){
+				// inside the setInterveral() block, `this` references the window object instead of the slideshow 
+				// container element, 
+				var $elem = $(this);
+				$elem.children().eq(0).appendTo($elem).show();
+				// iterate through the slides
+				setInterval(function(){
+					$elem.children().eq(0)
+					.hide().appendTo($elem)
+					.fadeIn(options.speed)
+				}, options.timeout);
+			});
+		}
+	}(jQuery));
+
+	$('.slide-container').slideshow({
+		'timeout' : 2000,
+		'speed' : 300
+	});
 });
+
