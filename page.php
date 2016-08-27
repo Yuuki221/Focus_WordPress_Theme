25bc91c673ad6e7fa86aa14fe941037e2eb63abe
Yuuki221 <xcicada824@gmail.com>
2016-08-27 16:25:31 -0400

    modified page file

--- page.php
+++ page.php
@@ -16,7 +16,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php
 			while ( have_posts() ) : the_post();
 
@@ -26,10 +25,8 @@ get_header(); ?>
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-
 			endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
