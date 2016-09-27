a5430fcdf7bbbd2289a1e10704e9d58ce731c34e
Yuuki221 <xcicada824@gmail.com>
2016-09-26 23:45:55 -0400

    add adjustment and modification

---
+++ page-about.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package non-profit-focus
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<section class="about-subsection aa-history">
+				<h2 class="about-subsec-heading">Africa America History</h2>
+					<h3> February is African American History Month</h3>
+					<p>
+						FOCUS+Pittsburgh joins with our Hill District Neighbor, Community Churches and Organizations, The City of Pittsburgh, The County of Allegheny, The Carnegie Library of Pittsburgh, The Pittsburgh School District, The Library of Congress, National Archives and Records Administration, National Endowment for the Humanities, National Gallery of Art, National Park Service, Smithsonian Institution and United States Holocaust Memorial Museum in paying tribute to the generations of African Americans who struggled with adversity to achieve full citizenship in American society.
+					</p>
+					<h3> Monday, February 1, 2016</h3>
+					<p>
+						St. Moses the Black the patron Saint of Non-violence.<br/>
+						<span>Saint Moses the Black</span> (330–405), (also known as <span>Abba Moses the Robber<span>, <span>the Abyssinian</span>, <span>the Ethiopian</span> and <span>the Strong</span>) was an ascetic monk and priest in Egypt in the fourth century AD, and a notable Desert Father.
+					</p>
+			</section>
+			<!-- section for Africa American History -->
+			<section class="about-subsection who-we-are">
+				<h2 class="about-subsec-heading">Who We Are</h2>
+					<h3>Fellowship of Orthodox Christians United to Serve (FOCUS)</h3>
+				<p>
+					FOCUS North America is a national movement of Orthodox Christians, united in faith and joined by a desire to provide action-oriented and sustainable solutions to poverty in communities across America—offering job training and placement, housing, and feeding programs, as well as other human services to the homeless and working poor of America. FOCUS also plays a vital role in bringing Orthodox Christians into direct relationship with the poor, positively impacting their own lives while helping those in need. In the past four years, FOCUS has grown into a vibrant and strong organization with eight flagship FOCUS Centers across the country and a dedicated volunteer corps of more than 6,000 people, with service outreach programs planned for 29 cities. FOCUS Pittsburgh’s offices are located on Centre Avenue in the Hill District. For more information about our national organization visit : 
+					<a href="http://focusnorthamerica.org">FOCUS North America HomePage</a>
+				</p>
+			</section><!-- sections for who we are  -->
+			<section class="about-subsection leadership">
+				<h2 class="about-subsec-heading">Leadership</h2>
+					<h3>Board of Directors</h3>
+						<ul>
+							<li>Mr.Eric Paljug, Ph.D, Chairman</li>
+							<li>Ms. Georgia Kazakis, Vice-Chair</li>
+							<li>Dr. Julie Papatheofanis, Secretary</li>
+							<li>Mrs.Lory Barsdate Easton</li>
+							<li>Dr. Nick Pandelidis</li>
+							<li>Mr. Brain Bullard</li>
+							<li>Dr. Maria Misthos-Cashy</li>
+						</ul><!-- board of directors list -->
+					<h3>Honorary Board Members</h3>
+						<ul>
+							<li>Mr. Charles R. Ajalat (Founder)</li>
+							<li>Mr.Willian Hoeft</li>
+						</ul><!-- honorary board members list -->
+					<h3>Advisory Board</h3>
+						<ul>
+							<li>Mr. Brian Gerich, CFO (retired) of Public Storage, Inc.</li>
+							<li>Andrew Natsios, Former head of the U.S. Agency for International Development</li>
+							<li>Peter Marudas, Former Chief of Staff to U.S. Senator Sarbanes</li>
+							<li>Maria Foscarinis, Founder & Executive Director of National Law Center on Homelessness & Poverty</li>
+							<li>Frederica Mathewes-Green, Nationally known author and commentator</li>
+							<li>Joel Smith, President of Comprehensive Media</li>
+							<li>Mrs. Theodora Polamalu, Principal of The Troy & Theodora Polamalu Foundation</li>
+						</ul><!-- advisory board list -->
+			</section><!-- sections for leadership -->
+			<section class="about-subsection staff-directory">
+				<h2 class="about-subsec-heading">Staff Directory</h2>
+				<!-- table goes here -->
+			</section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
---
+++ page-donate.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying donate pages.
+ *
+ * This is the template that displays donate page.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package non-profit-focus
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
